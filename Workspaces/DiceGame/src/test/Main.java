f66331d84b9520661ee319d455b88e31ef6d84a8
U22229 <U22229@E906LLAX.ustr.com>
2016-12-18 17:56:27 -0600

    dice game - initial commit.

---
+++ Workspaces/DiceGame/src/test/Main.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Main {
+	
+	
+	/*•	A set of three matching dice earns:
+		•	Ones [1,1,1]   => 1000 points
+		•	Twos [2,2,2]   => 200
+		•	Threes [3,3,3] => 300
+		•	Fours [4,4,4]   => 400
+		•	Fives [5,5,5]   => 500
+		•	Sixes [6,6,6]   => 600
+		•	Additionally, each dice NOT part of a matching set of three earns:
+		•	Ones => 100 points each
+		•	Fours => 40 each
+		•	Any other roll (2s, 3s, 5s, 6s) => 0 each*/
+
+	
+
+}

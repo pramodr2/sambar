09e8624d499eb1f849a782637f27826f19887c3f
U22229 <U22229@E906LLAX.ustr.com>
2016-12-20 00:12:01 -0600

    dice game - match and getPoints methods..

--- Workspaces/TestDiceGame/src/test/Sides.java
+++ Workspaces/TestDiceGame/src/test/Sides.java
@@ -4,25 +4,62 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-
 class Sides {
 	
 	List<Integer> sides;
 	Integer points=0;
-	
-	public List<Integer> getSides() {
-		return sides;
-	}
-	public void setSides(List<Integer> sides) {
-		this.sides = sides;
-	}
-	public Integer getPoints() {
-		return points;
-	}
-	public void setPoints(Integer points) {
-		this.points = points;
+
+	private Integer get(Integer i) {
+		return sides.get(i);
 	}
 
+	public Integer getPoints() {
+		
+		if (match(new Sides(1,1,1)) == 0) {
+			remove(new Sides(1,1,1));
+			points=1000;
+		} else if (match(new Sides(2,2,2)) == 0) {
+			points=200;
+			remove(new Sides(2,2,2));
+		} else if (match(new Sides(3,3,3)) == 0) {
+			remove(new Sides(3,3,3));
+			points=300;
+		} else if (match(new Sides(4,4,4)) == 0) {
+			remove(new Sides(4,4,4));
+			points=400;
+		} else if (match(new Sides(5,5,5)) == 0) {
+			remove(new Sides(5,5,5));
+			points=500;
+		} else if (match(new Sides(6,6,6)) == 0) {
+			remove(new Sides(6,6,6));
+			points=600;
+		}
+		
+		
+		for (int i=0;i<size();i++) {
+				
+			switch(get(i)) {
+			case 1:
+				points+=100;
+				break;
+			case 4:
+				points+=40;
+				break;
+			default:
+				points+=0;
+				break;
+			}
+			
+		}
+		
+		return points;
+		
+	}
+
+	private void remove(Sides sides2) {
+		
+	}
+	
 	public Sides(Integer... i) {
 		sides = new ArrayList<>();
 		for (int ii = 0;ii<i.length;ii++)
@@ -31,9 +68,20 @@ class Sides {
 		Collections.sort(sides);
 	}
 	
-	public Integer getPoints(Sides result) {
-		return points;
+	private Integer size() {
+		return sides.size();
 	}
+	
+
+	public String toString() {
+		return ""+sides+"\n"+points;
+	}
+	
+	private Integer match(Sides s) {
+		
+		return 1;
+	}
+	
 }
 
 
--- Workspaces/TestDiceGame/src/test/TestMain.java
+++ Workspaces/TestDiceGame/src/test/TestMain.java
@@ -12,8 +12,31 @@ public class TestMain {
 		
 	
 		Sides sides = new Sides(1,1,1);
-		assertEquals(sides.getPoints(sides), new Integer(1000));
+		assertEquals(sides.getPoints(), new Integer(1000));
 		
+		
+		sides = new Sides(1,2,3,4,5);
+		assertEquals(sides.getPoints(), new Integer(140));
+
+		
+		sides = new Sides(1,1,1,2,2);
+		assertEquals(sides.getPoints(), new Integer(1000));
+
+		
+		sides = new Sides(5,4,5,4,5);
+		assertEquals(sides.getPoints(), new Integer(580));
+		
+		sides = new Sides(4,4,4,4,4);
+		assertEquals(sides.getPoints(), new Integer(480));
+		
+		sides = new Sides(1,5,5,5,1);
+		assertEquals(sides.getPoints(), new Integer(700));
+		
+		sides = new Sides(2,2,2,2,2);
+		assertEquals(sides.getPoints(), new Integer(200));
+		
+		sides = new Sides(1,1);
+		assertEquals(sides.getPoints(), new Integer(200));
 		
 		
 	}

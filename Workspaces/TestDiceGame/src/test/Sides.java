bb86582b70fd4eab3a5a219d55ddb74ecfcf23df
U22229 <U22229@E906LLAX.ustr.com>
2016-12-20 00:58:31 -0600

    add match and remove methods

--- Workspaces/TestDiceGame/src/test/Sides.java
+++ Workspaces/TestDiceGame/src/test/Sides.java
@@ -13,6 +13,19 @@ class Sides {
 		return sides.get(i);
 	}
 
+	/**
+	 * 
+	 * STEP 1:
+	 * 
+	 * Get points for each sequence identified in result...
+	•	Ones [1,1,1]   => 1000 points
+	•	Twos [2,2,2]   => 200
+	•	Threes [3,3,3] => 300
+	•	Fours [4,4,4]   => 400
+	•	Fives [5,5,5]   => 500
+	•	Sixes [6,6,6]   => 600
+
+	 */
 	public Integer getPoints() {
 		
 		if (match(new Sides(1,1,1)) == 0) {
@@ -35,7 +48,15 @@ class Sides {
 			points=600;
 		}
 		
+		/* STEP 2:
+		After removing the matching sequence
+		Calculate the remaining points
 		
+		•	Ones => 100 points each
+		•	Fours => 40 each
+		•	Any other roll (2s, 3s, 5s, 6s) => 0 each
+		 */
+	
 		for (int i=0;i<size();i++) {
 				
 			switch(get(i)) {
@@ -55,9 +76,18 @@ class Sides {
 		return points;
 		
 	}
+	
+	/**
+	 * Remove sublist [1,1,1] from the input after points calculated.
+	 */
 
 	private void remove(Sides sides2) {
-		
+		int idx = Collections.indexOfSubList(this.sides, sides2.sides);
+		if ( idx >= 0) {
+			for (int i=idx;i<idx+3;i++) {
+				sides.remove(idx);
+			}
+		}
 	}
 	
 	public Sides(Integer... i) {
@@ -77,7 +107,21 @@ class Sides {
 		return ""+sides+"\n"+points;
 	}
 	
+	/**
+	 * Check if the input has a sequence in it.- [1,1,1]  [2,2,2] etc..
+	 * If yes, return 0, else 1
+	 * @param s
+	 * @return
+	 */
 	private Integer match(Sides s) {
+		
+		if (this.sides.size()>=3) {
+			for (int i=0;i<=2;i++) {
+				if (s.sides.subList(0, 3).equals(this.sides.subList(i, i+3))) {
+					return 0;
+				} 
+			}
+		}
 		
 		return 1;
 	}
